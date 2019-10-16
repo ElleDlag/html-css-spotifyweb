03e568b5d596ea47e82320f88f42ae3110253a48
ElleDlag <38381770+ElleDlag@users.noreply.github.com>
2019-10-17 00:08:09 +0200

    Fix Main Nav script

--- BonusPro/assets/js/script.js
+++ BonusPro/assets/js/script.js
@@ -2,26 +2,16 @@ window.onload = () => {
     var dom = document;
     var listItem = dom.querySelectorAll(".main_nav-link");
 
-    function addClass(e){
-        e.target.classList.add("active");
-    }
     function removeClass(){
         for (let i = 0; i < listItem.length; i++) {
         listItem[i].classList.remove("active");
         }
     }
-    function removeClassHover(e){
-        e.target.classList.remove("active");
-    }
     function addClassOk(e){
-        e.target.removeEventListener("mouseout", removeClassHover);
         e.target.classList.add("active");
     }
-
     listItem.forEach((e) => {
             e.addEventListener("mouseover", removeClass);
-            e.addEventListener("mouseover", addClass);
-            e.addEventListener("mouseout", removeClassHover);
             e.addEventListener("click", addClassOk);
         }
     );
